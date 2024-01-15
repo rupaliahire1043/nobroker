07908b9247cebd605cf170a793ef16df50f4bc9d
prana <rupaliahire1043@gmail.com>
2024-01-15 16:09:14 +0530

    5th jan 2024

---
+++ nobroker/src/main/java/com/nobroker/service/OwnerPlanServices.java
@@ -0,0 +1,8 @@
+package com.nobroker.service;
+
+import com.nobroker.payload.OwnerPlanDto;
+
+public interface OwnerPlanServices {
+
+    public OwnerPlanDto createOwnerPlans(OwnerPlanDto ownerPlanDto);
+}
